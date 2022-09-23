39cfe9c2ed4ec316a70fa6296a5f687b4846b12e
Serg <kohan_sergey@ukr.net>
2022-09-23 14:34:01 +0300

    ch

--- src/test/java/BookingTest/Test/TestInit.java
+++ src/test/java/BookingTest/Test/TestInit.java
@@ -3,18 +3,13 @@ package BookingTest.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.awt.*;
-import java.util.concurrent.TimeUnit;
 
 public class TestInit {
     public WebDriver driver;
 
     @BeforeMethod
-    @Deprecated
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
